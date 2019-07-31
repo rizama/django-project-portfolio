7fb4256549e9b23261326939d5ffcaf009bee137
rizkysampratama <rizkysampratama@gmail.com>
2019-07-31 17:26:21 +0700

    Added Detail Blog to Blog Apps

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body     = models.TextField()
     image    = models.ImageField(upload_to = 'images/')
+
+    # Merubah Judul Objek pada Admin Page 
+    def __str__(self):
+        return self.title
+
+    # Membuat Preview Content Post dilimit
+    def summary(self):
+        return self.body[:100]
+
+    # Membuat Format Waktu pada Blog
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
